08fd24e990b23f3839a2fe49f9758bab8cdd7eb4
truongbk2010 <truongbk10@gmail.com>
2017-02-12 01:30:29 +0700

    modify response

--- test.js
+++ test.js
@@ -3,7 +3,8 @@ var app = require('express')(),
    port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname + "/demo.html");
+    //res.sendFile(__dirname + "/demo.html");
+    res.json({notes: "This is my notes!"})
 })
 
 app.listen(port, function() {
